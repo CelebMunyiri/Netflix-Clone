87a57d9173186389fa4dae8ae49907efeb301dc1
David Munyiri <davidmumyiri2019@gmail.com>
2022-12-06 07:30:34 -0800

    did a change

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const open = document.querySelectorAll(".open");
 const it = document.querySelectorAll(".it");
 const buton = document.querySelectorAll(".btn");
 const butn = document.querySelectorAll(".butn");
+const getStarted = document.querySelectorAll(".get");
+const sign = document.querySelectorA(".sign");
 
 for (let i = 0; i < buton.length; i++)
   for (let i = 0; i < it.length; i++)
-    buton[i].addEventListener("click", function () {
-      it[i].classList.remove("hidden");
-      buton[i].classList.add("hidden");
-      butn[i].classList.remove("hidden");
-      for (let i = 0; i < open.length; i++) open[i].style.margin = "10px";
-      it[i].style.margin = "10px";
-      for (let i = 0; i < butn.length; i++)
-        butn[i].addEventListener("click", function () {
-          buton[i].classList.remove("hidden");
-          butn[i].classList.add("hidden");
-          it[i].classList.add("hidden");
-        });
-    });
+    for (let i = 0; i < getStarted.length; i++)
+      buton[i].addEventListener("click", function () {
+        it[i].classList.remove("hidden");
+        buton[i].classList.add("hidden");
+        butn[i].classList.remove("hidden");
+        for (let i = 0; i < open.length; i++) open[i].style.margin = "10px";
+        it[i].style.margin = "10px";
+        for (let i = 0; i < butn.length; i++)
+          butn[i].addEventListener("click", function () {
+            buton[i].classList.remove("hidden");
+            butn[i].classList.add("hidden");
+            it[i].classList.add("hidden");
+          });
+      });
+getStarted[i].addEventListener("click", function () {
+  sign.classList.add("hidden");
+});
