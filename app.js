1987b45b8881c6fadae8326eb4910eb374a9b2d8
David Munyiri <davidmumyiri2019@gmail.com>
2022-12-07 07:59:23 -0800

    one chnge on 7th dec 22

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ const sign = document.querySelector(".sign");
 
 for (let i = 0; i < buton.length; i++)
   for (let i = 0; i < it.length; i++)
-    for (let i = 0; i < getStarted.length; i++)
-      buton[i].addEventListener("click", function () {
-        it[i].classList.remove("hidden");
-        buton[i].classList.add("hidden");
-        butn[i].classList.remove("hidden");
-        for (let i = 0; i < open.length; i++) open[i].style.margin = "10px";
-        it[i].style.margin = "10px";
-        for (let i = 0; i < butn.length; i++)
-          butn[i].addEventListener("click", function () {
-            buton[i].classList.remove("hidden");
-            butn[i].classList.add("hidden");
-            it[i].classList.add("hidden");
-          });
-      });
+    buton[i].addEventListener("click", function () {
+      it[i].classList.remove("hidden");
+      buton[i].classList.add("hidden");
+      butn[i].classList.remove("hidden");
+      for (let i = 0; i < open.length; i++) open[i].style.margin = "10px";
+      it[i].style.margin = "10px";
+      for (let i = 0; i < butn.length; i++)
+        butn[i].addEventListener("click", function () {
+          buton[i].classList.remove("hidden");
+          butn[i].classList.add("hidden");
+          it[i].classList.add("hidden");
+        });
+    });
 getStarted[i].addEventListener("click", function () {
-  sign.classList.add("hidden");
+  for (let i = 0; i < getStarted.length; i++) {
+    sign.classList.toggle("hidden");
+  }
 });
