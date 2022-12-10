33d51cc7edcdc24282edaa12f63bdae9fe871459
David Munyiri <davidmumyiri2019@gmail.com>
2022-12-10 08:29:35 -0800

    did code refactoring to enhance DRY principles

--- app.js
+++ app.js
@@ -2,30 +2,23 @@
 const open = document.querySelectorAll(".open");
 const it = document.querySelectorAll(".it");
 const buton = document.querySelectorAll(".btn");
-const butn = document.querySelectorAll(".butn");
+
 const getStarted = document.querySelectorAll(".get");
 const sign = document.querySelector(".form");
 
 //implementing the buttons on FAQ page
 for (let i = 0; i < buton.length; i++)
-  for (let i = 0; i < it.length; i++)
-    buton[i].addEventListener("click", function () {
-      it[i].classList.remove("hidden");
-      buton[i].classList.add("hidden");
-      butn[i].classList.remove("hidden");
-      for (let i = 0; i < open.length; i++) open[i].style.margin = "10px";
-      it[i].style.margin = "10px";
-      for (let i = 0; i < butn.length; i++)
-        butn[i].addEventListener("click", function () {
-          buton[i].classList.remove("hidden");
-          butn[i].classList.add("hidden");
-          it[i].classList.add("hidden");
-        });
-    });
+  buton[i].addEventListener("click", function () {
+    it[i].classList.toggle("hidden");
+    if (it[i].classList.contains("hidden")) buton[i].style.rotate = "-45deg";
+    else {
+      buton[i].style.rotate = "45deg";
+    }
+  });
 //implementing the getstarted button
 for (let i = 0; i < getStarted.length; i++)
   getStarted[i].addEventListener("click", function () {
     console.log("button clicked");
     //sign.style.background = "red";
-    sign.classList.remove("hidden");
+    sign.classList.toggle("hidden");
   });
